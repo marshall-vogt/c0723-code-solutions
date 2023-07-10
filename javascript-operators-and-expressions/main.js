26ef3b527fbdede3aed831a87fc4c52a40ce4349
Marshall Vogt <vogt_marshall@yahoo.com>
2023-07-10 22:26:29 +0000

    Completed operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,53 @@
+// i
+const width = 17;
+const height = 33;
+const area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+// ii
+const bill = 288;
+const payment = 292;
+const change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+// iii
+const quizzes = 150;
+const midterm = 250;
+const final = 350;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+// iv
+const firstName = 'Marshall';
+const lastName = 'Vogt';
+const fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+// v
+const pH = 9.2;
+const isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+// vi
+const headCount = 37;
+const isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+// vii
+let motto = fullName;
+motto += ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
