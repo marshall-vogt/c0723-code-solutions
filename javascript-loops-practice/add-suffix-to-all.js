97e8c6c886565d2988057f4dd4aef2c2e465b91a
Marshall Vogt <vogt_marshall@yahoo.com>
2023-07-13 19:24:33 +0000

    Updated javascript-loops-practice exercise

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -3,8 +3,7 @@
 function addSuffixToAll(words, suffix) {
   const suffixArray = [];
   for (let i = 0; i < words.length; i++) {
-    words[i] += suffix;
-    suffixArray.push(words[i]);
+    suffixArray.push((words[i] += suffix));
   }
   return suffixArray;
 }
