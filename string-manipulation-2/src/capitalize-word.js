e1046c46aee64ba80617ec677fdeac1405676fb9
Marshall Vogt <vogt_marshall@yahoo.com>
2023-07-14 17:36:27 +0000

    Updated capitalize word function

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -3,13 +3,7 @@
 function capitalizeWord(word) {
   const lowerCaseWord = word.toLowerCase();
   if (lowerCaseWord === 'javascript') {
-    const java = lowerCaseWord.slice(0, 4);
-    const script = lowerCaseWord.slice(4);
-    const j = java.slice(0, 1);
-    const ava = java.slice(1);
-    const s = script.slice(0, 1);
-    const cript = script.slice(1);
-    return j.toUpperCase() + ava + s.toUpperCase() + cript;
+    return 'JavaScript';
   } else {
     const firstLetter = word.slice(0, 1);
     const restOfWord = word.slice(1);
