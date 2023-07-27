02e094d0695143a034c740951573d642b7c1a7a4
Marshall Vogt <vogt_marshall@yahoo.com>
2023-07-27 20:42:45 +0000

    Updated variable name

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -5,11 +5,8 @@ function handleClick(event) {
   console.log('event.target: ', event.target);
   console.log('event.target.tagName: ', event.target.tagName);
   if (event.target && event.target.tagName === 'BUTTON') {
-    const closestItem = event.target.closest('.task-list-item');
-    console.log(
-      'closest .task-list-item: ',
-      event.target.closest('.task-list-item')
-    );
-    closestItem.remove();
+    const $closestItem = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item: ', $closestItem);
+    $closestItem.remove();
   }
 }
