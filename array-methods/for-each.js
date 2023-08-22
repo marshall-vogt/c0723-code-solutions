813b6ddb3c238aafe8420f2725122b43d09cc90c
Marshall Vogt <vogt_marshall@yahoo.com>
2023-08-22 17:38:11 +0000

    Added quiz questions and fixed return object in map.js

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -1,6 +1,6 @@
 const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 
 values.forEach((value) => console.log(value));
-values.forEach((value) =>
-  console.log(values[values.length - 1 - values.indexOf(value)])
+values.forEach((value, index, values) =>
+  console.log(values[values.length - 1 - index])
 );
--- array-methods/map.js
+++ array-methods/map.js
@@ -1,8 +1,9 @@
 const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
 
-const priceObjects = prices.map(
-  (price) => `price: ${price}, salePrice: ${price / 2}`
-);
+const priceObjects = prices.map((originalPrice) => ({
+  price: originalPrice,
+  salePrice: originalPrice / 2,
+}));
 console.log('Price objects', priceObjects);
 
 const formattedPrices = prices.map((price) => `$${price.toFixed(2)}`);
