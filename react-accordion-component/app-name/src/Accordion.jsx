994e2be58b23fe7285e227df13b7fcf1fe767939
Marshall Vogt <vogt_marshall@yahoo.com>
2023-09-01 18:34:54 +0000

    Completed react-accordion-component challenge

---
+++ react-accordion-component/app-name/src/Accordion.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './Accordion.css';
+
+export default function Accordion({ topics }) {
+  const [topicClickedIndex, setTopicClickedIndex] = useState(undefined);
+  const accordion = topics.map((topic, index) => (
+    <div key={index} className="topic">
+      <div
+        className="header"
+        onClick={() =>
+          topicClickedIndex === index
+            ? setTopicClickedIndex(undefined)
+            : setTopicClickedIndex(index)
+        }>
+        <Topic subject={topic.title} topicIndex={index} />
+      </div>
+      <div
+        className="info"
+        style={{ display: topicClickedIndex === index ? 'flex' : 'none' }}>
+        <Info info={topic.content} />
+      </div>
+    </div>
+  ));
+  return <div className="container">{accordion}</div>;
+}
+
+function Topic({ subject }) {
+  return <>{subject}</>;
+}
+
+function Info({ info }) {
+  return <>{info}</>;
+}
