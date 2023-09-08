d3ffe0205217c8c760721e1dc1cbfae2cad3ece6
Marshall Vogt <vogt_marshall@yahoo.com>
2023-09-08 18:34:01 +0000

    Reduced unnecessary code in .js files

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,8 +1,7 @@
 import { readFile } from 'node:fs/promises';
 
 try {
-  const anyText = new URL(process.argv[2], import.meta.url);
-  const contents = await readFile(anyText, { encoding: 'utf8' });
+  const contents = await readFile(process.argv[2], { encoding: 'utf8' });
   console.log(contents);
 } catch (error) {
   console.log(error);
--- node-fs-readfile/read-dijkstra.js
+++ node-fs-readfile/read-dijkstra.js
@@ -1,8 +1,7 @@
 import { readFile } from 'node:fs/promises';
 
 try {
-  const dijkstraText = new URL('./dijkstra.txt', import.meta.url);
-  const contents = await readFile(dijkstraText, { encoding: 'utf8' });
+  const contents = await readFile('dijkstra.txt', 'utf8');
   console.log(contents);
 } catch (error) {
   console.log(error);
