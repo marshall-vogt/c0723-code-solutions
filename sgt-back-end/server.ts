bb5af9bb84eefd321d4d495b71eca33c965ae003
Marshall Vogt <vogt_marshall@yahoo.com>
2023-09-19 03:50:23 +0000

    Assigned functions for redundant code and removed template literals from paramaters

--- sgt-back-end/server.ts
+++ sgt-back-end/server.ts
@@ -4,6 +4,7 @@ import { ClientError } from './client-error.js';
 import { errorMiddleware } from './error-middleware.js';
 
 const app = express();
+app.use(express.json());
 
 // only create ONE pool for your whole server
 const db = new pg.Pool({
@@ -14,15 +15,40 @@ const db = new pg.Pool({
   },
 });
 
-// type Grade = {
-//   course: string;
-//   createdAt: string;
-//   gradeId: number;
-//   name: string;
-//   score: number;
-// };
+type Grade = {
+  course: string;
+  createdAt: string;
+  gradeId: number;
+  name: string;
+  score: number;
+};
 
-app.use(express.json());
+function nameVerify(name: string) {
+  if (name === undefined) {
+    throw new ClientError(400, 'name is a required field');
+  }
+}
+
+function courseVerify(course: string) {
+  if (course === undefined) {
+    throw new ClientError(400, 'course is a required field');
+  }
+}
+
+function scoreVerify(score: number) {
+  if (score === undefined) {
+    throw new ClientError(400, 'score is a required field');
+  }
+  if (Number.isNaN(score) || score > 100 || score < 0) {
+    throw new ClientError(400, 'score should be an integer value from 0-100');
+  }
+}
+
+function idVerify(id: number) {
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
+    throw new ClientError(400, 'id must be a positive integer');
+  }
+}
 
 app.get('/api/grades', async (req, res, next) => {
   try {
@@ -31,8 +57,8 @@ app.get('/api/grades', async (req, res, next) => {
         from "grades";
     `;
 
-    const grades = await db.query(sql);
-    res.json(grades.rows);
+    const grades = await db.query<Grade>(sql);
+    res.status(200).json(grades.rows);
   } catch (err) {
     next(err);
   }
@@ -41,26 +67,17 @@ app.get('/api/grades', async (req, res, next) => {
 app.post('/api/grades', async (req, res, next) => {
   try {
     const { name, course, score } = req.body;
-    if (name === undefined) {
-      throw new ClientError(400, 'name is a required field');
-    }
-    if (course === undefined) {
-      throw new ClientError(400, 'course is a required field');
-    }
-    if (score === undefined) {
-      throw new ClientError(400, 'score is a required field');
-    }
-    if (isNaN(score) || score > 100 || score < 0) {
-      throw new ClientError(400, 'score should be a value from 0-100');
-    }
-    const values = [`${name}`, `${course}`, `${score}`];
+    const values = [name, course, score];
     const sql = `
       insert into "grades" ("name", "course", "score")
         values ($1, $2, $3)
         returning *;
     `;
-    const grades = await db.query(sql, values);
-    res.json(grades.rows[0]);
+    nameVerify(name);
+    courseVerify(course);
+    scoreVerify(score);
+    const grades = await db.query<Grade>(sql, values);
+    res.status(201).json(grades.rows[0]);
   } catch (err) {
     next(err);
   }
@@ -70,7 +87,7 @@ app.put('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const { name, course, score } = req.body;
     const id = Number(req.params.gradeId);
-    const values = [`${name}`, `${course}`, `${score}`, `${id}`];
+    const values = [name, course, score, id];
     const sql = `
       update "grades"
         set "name" = $1,
@@ -79,34 +96,15 @@ app.put('/api/grades/:gradeId', async (req, res, next) => {
         where "gradeId" = $4
         returning *
     `;
-    if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
-      throw new ClientError(400, 'id must be a positive integer');
-    }
-    if (name === undefined) {
-      throw new ClientError(400, 'name is a required field');
-    }
-    if (course === undefined) {
-      throw new ClientError(400, 'course is a required field');
-    }
-    if (
-      score === undefined ||
-      Number.isNaN(score) ||
-      score < 0 ||
-      score > 100
-    ) {
-      throw new ClientError(
-        400,
-        'score is a required field and must be a positive integer between 0 and 100'
-      );
-    }
-    if (Number.isNaN(id) || !Number.isInteger(id)) {
-      throw new ClientError(400, 'id must be a positive integer');
-    }
-    const grades = await db.query(sql, values);
+    idVerify(id);
+    nameVerify(name);
+    courseVerify(course);
+    scoreVerify(score);
+    const grades = await db.query<Grade>(sql, values);
     if (grades.rows.length === 0) {
       throw new ClientError(404, `cannot find note with id ${id}`);
     }
-    res.json(grades.rows[0]);
+    res.status(200).json(grades.rows[0]);
   } catch (err) {
     next(err);
   }
@@ -115,16 +113,14 @@ app.put('/api/grades/:gradeId', async (req, res, next) => {
 app.delete('/api/grades/:gradeId', async (req, res, next) => {
   try {
     const id = Number(req.params.gradeId);
-    if (Number.isNaN(id) || !Number.isInteger(id) || id < 1) {
-      throw new ClientError(400, 'id must be a positive integer');
-    }
-    const values = [`${id}`];
+    idVerify(id);
+    const values = [id];
     const sql = `
     delete
       from "grades"
       where "gradeId" = $1
     `;
-    const grades = await db.query(sql, values);
+    const grades = await db.query<Grade>(sql, values);
     if (grades.rowCount === 0) {
       throw new ClientError(404, `cannot find note with id ${id}`);
     }
@@ -135,7 +131,6 @@ app.delete('/api/grades/:gradeId', async (req, res, next) => {
 });
 
 app.use(errorMiddleware);
-
 app.listen('8080', () => {
   console.log('Listening on port 8080');
 });
