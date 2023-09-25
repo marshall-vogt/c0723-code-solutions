c9c373a5c90ba536722091641976f6db9ae96f46
Marshall Vogt <vogt_marshall@yahoo.com>
2023-09-25 18:41:11 +0000

    Added link to NotFound component

--- react-routing/client/src/pages/NotFound.tsx
+++ react-routing/client/src/pages/NotFound.tsx
@@ -1,4 +1,5 @@
 import './NotFound.css';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
@@ -7,7 +8,9 @@ export default function NotFound() {
         <div className="col text-center mb-5">
           <h3>Uh oh, we could not find the page you were looking for!</h3>
           {/* TODO: Make this a link to the Catalog, with className "text-muted" */}
-          <p className="text-muted">Return to the catalog</p>
+          <p className="text-muted">
+            <Link to="catalog">Return to the catalog</Link>
+          </p>
         </div>
       </div>
     </div>
